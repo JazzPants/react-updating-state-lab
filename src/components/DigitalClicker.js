78120f1d82f9dda4855c8ff01fa69ea172dca496
JazzPants <leighton.l14@gmail.com>
2022-01-04 23:14:18 +1100

    Done!

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,34 @@
 // Code DigitalClicker Component Here
+import React, {Component} from 'react'
+
+class DigitalClicker extends Component {
+    constructor() {
+        super();
+        this.state = {
+            timesClicked: 0
+        }
+    }
+    // increment = () => { //persist function
+    //     const newCount = this.state.timesClicked + 1
+    //     this.setState({ //asynchronous
+    //         count: newCount
+    //     })
+    // }
+
+    handleClick = () => {
+        this.setState(prevState => ({
+            timesClicked: prevState.timesClicked + 1
+        }))
+    }
+
+    render() {
+        return(<button onClick={this.handleClick}>
+            {this.state.timesClicked}
+            </button>)
+        
+        }
+}
+
+
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,63 @@
 // Code YouTubeDebugger Component Here
+import React, {Component} from 'react'
+
+class YouTubeDebugger extends Component {
+    constructor() {
+        super();
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+        }
+        console.log(this.state.settings.bitrate)
+    }
+    
+
+    handleChangeBitRate = () => {
+        this.setState({
+            settings : 
+            {...this.state.settings, bitrate: 12}
+        }, () => console.log(this.state.settings.bitrate))
+
+    }
+
+    handleChangeResolution = () => {
+        this.setState({settings: {
+            ...this.state.settings,
+            video: { //updating a nest state
+                ...this.state.settings.video, //if we don't use spread operator, it'll remove other properties in the video object!
+                resolution: '720p'
+            }
+
+        }}, () => console.log(this.state.settings.video.resolution))
+    }
+
+    handleChangeUsers = () => {
+        this.setState({
+            //...this.state, \still works\
+            user: 'John'
+        }, () => console.log(this.state.user))
+    }
+
+render() {
+    return(
+        <div>
+            <button className='bitrate' onClick={this.handleChangeBitRate}>Change bitrate</button>
+
+            <button className='resolution' onClick={this.handleChangeResolution}>Change resolution</button>
+
+            <button className='user' onClick={this.handleChangeUsers}>Change users</button>
+        </div>
+    )
+}
+
+}
+
+
+
+export default YouTubeDebugger;
